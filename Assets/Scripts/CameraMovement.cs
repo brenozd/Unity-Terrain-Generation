7659be55f8265d97f5ffe32fb20216df9d1347f4
Breno Zanato Detomini <brenodetominI@hotmail.com>
2020-05-14 20:04:16 -0400

    Started Working on Mesh Splitting

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float moveSpeed = 0.5f;
+    public float scrollSpeed = 10.0f;
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    void Update()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+            {
+                transform.position += moveSpeed * new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+            }
+
+            if (Input.GetAxis("Mouse ScrollWheel") != 0)
+            {
+                transform.position += scrollSpeed * new Vector3(0, -Input.GetAxis("Mouse ScrollWheel"), 0);
+            }
+
+            yaw += speedH * Input.GetAxis("Mouse X");
+            pitch -= speedV * Input.GetAxis("Mouse Y");
+
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked) { Cursor.lockState = CursorLockMode.None; Cursor.visible = true; }
+            if (Cursor.lockState == CursorLockMode.None) { Cursor.lockState = CursorLockMode.Locked; Cursor.visible = false; }
+        }
+    }
+}
--- Assets/Scripts/Terrain Generation/MeshTerrainGenerator.cs
+++ Assets/Scripts/Terrain Generation/MeshTerrainGenerator.cs
@@ -4,57 +4,88 @@ using UnityEngine;
 
 public class MeshTerrainGenerator : MonoBehaviour
 {
-    public int size = 128;
+    public int size = 512;
     public Material material;
-    Vector3[] vertex;
-    int[] triangles;
+    struct MeshComponents
+    {
+        public Vector3[] vertices;
+        public int[] triangles;
+    }
 
 
     private void Start()
     {
-        generateMesh();
+        Mesh[] meshes = SplitMeshes(CalculateMesh(size), size);
+        foreach (Mesh m in meshes)
+        {
+            m.RecalculateNormals();
+            m.RecalculateBounds();
+
+            GameObject go = new GameObject("Mesh");
+            go.transform.parent = this.transform;
+            go.AddComponent<MeshFilter>();
+            go.AddComponent<MeshRenderer>();
+            go.GetComponent<Renderer>().material = material;
+            go.GetComponent<MeshFilter>().mesh = m;
+
+            //go.transform.localPosition = new Vector3(-size / 2, -size / 2, -size / 2);
+        }
     }
-    void generateMesh()
+
+    MeshComponents CalculateMesh(int size)
     {
-        vertex = new Vector3[(size + 1)*(size+1)];
+        MeshComponents mesh = new MeshComponents();
+        mesh.vertices = new Vector3[(size + 1) * (size + 1)];
+        mesh.triangles = new int[size * size * 6];
+
         for (int i = 0, x = 0; x <= size; x++)
         {
             for (int z = 0; z <= size; z++, i++)
             {
-                float y = Noise.PerlinNoise2D(x *.15f, z*.15f) * 2f;
-                vertex[i] = (new Vector3(x, y, z));
+                float y = Noise.PerlinNoise2D(x * 01f, z * 1f) * 1;
+                mesh.vertices[i] = (new Vector3(x, y, z));
             }
         }
 
-        triangles = new int[size * size * 6];
+        return mesh;
+    }
 
-        int ti = 0;
-        int vi = 0;
-        for (int y = 0; y < size; y++, vi++) {
-			for (int x = 0; x < size; x++) {
-				triangles[ti] = vi;
-				triangles[ti + 3] = triangles[ti + 2] = vi + 1;
-				triangles[ti + 4] = triangles[ti + 1] = vi + size + 1;
-				triangles[ti + 5] = vi + size + 2;
-                ti+=6;
-                vi++;
-			}
-		}
+    Mesh[] SplitMeshes(MeshComponents mesh, int size, int vertsPerMesh = 65534)
+    {
+        int numMeshes = mesh.vertices.Length / vertsPerMesh + 1;
+        Mesh[] returnMeshes = new Mesh[numMeshes];
 
-        Mesh mesh = new Mesh();
-        mesh.SetVertices(vertex);
-        mesh.SetTriangles(triangles, 0);
-        mesh.RecalculateBounds();
-        //Normals are inverted
-        mesh.RecalculateNormals();
+        for (int m = 0; m < numMeshes; m++)
+        {
+            returnMeshes[m] = new Mesh();
+            List<Vector3> mVertices = new List<Vector3>();
+            List<int> mTriangles = new List<int>();
+            int index = 0;
 
-        GameObject go = new GameObject("Mesh");
-        go.transform.parent = this.transform;
-        go.AddComponent<MeshFilter>();
-        go.AddComponent<MeshRenderer>();
-        go.GetComponent<Renderer>().material = material;
-        go.GetComponent<MeshFilter>().mesh = mesh;
+            for (int v = 0; v < vertsPerMesh; v++)
+            {
+                index = m * vertsPerMesh + v;
+                if (index < mesh.vertices.Length)
+                    mVertices.Add(mesh.vertices[index]);
+            }
 
-        go.transform.localPosition = new Vector3(-size / 2, -size / 2, -size / 2);
+            for (int v = 0, x = 0; x < (mVertices.Count / (size+1)) - 1; x++)
+            {
+                for (int z = 0; z < size; z++, v++)
+                {
+                    mTriangles.Add(v);
+                    mTriangles.Add(v + 1);
+                    mTriangles.Add(v + size + 1);
+                    mTriangles.Add(v + size + 1);
+                    mTriangles.Add(v + 1);
+                    mTriangles.Add(v + size + 2);
+                }
+                v++;
+            }
+            returnMeshes[m].SetVertices(mVertices);
+            returnMeshes[m].SetTriangles(mTriangles, 0);
+        }
+
+        return returnMeshes;
     }
 }
